168af7202e6cc13bcacf1de540144ef592eb1477
Andrew Famiglietti <afamiglietti@gmail.com>
2017-07-07 20:20:21 -0400

    smallest error

--- formcatcher.php
+++ formcatcher.php
@@ -2,6 +2,6 @@
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
   if( ini_get('allow_url_fopen') ) {
-   echo "yes!!"
+   echo "yes!!";
 }
 ?>
