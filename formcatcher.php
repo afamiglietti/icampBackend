f195299fb4d688a6d7cc254a042d4c5644d9a716
Andrew Famiglietti <afamiglietti@gmail.com>
2017-07-07 20:34:29 -0400

    append

--- formcatcher.php
+++ formcatcher.php
@@ -5,7 +5,7 @@
   $apiString = "WHERE Category IN (sports, media)";
   $apiURL = urlencode($apiString);
 
-  $my_var = file_get_contents("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+ROWID%2C+Name%2C+Description+FROM+1Iw9qt8__F6E8RB53bbflrGAt3eI4GS3StvcF1o-I%20" + $apiURL + "&key=AIzaSyBYdqXls8CORbjl1yVC53qZ-75pMJaCmJY");
+  $my_var = file_get_contents("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+ROWID%2C+Name%2C+Description+FROM+1Iw9qt8__F6E8RB53bbflrGAt3eI4GS3StvcF1o-I%20" . $apiURL . "&key=AIzaSyBYdqXls8CORbjl1yVC53qZ-75pMJaCmJY");
 
   echo $my_var;
 ?>
