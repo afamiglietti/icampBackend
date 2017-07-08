acbeec92b634d1ec44f4bf76482423b4479c159e
Andrew Famiglietti <afamiglietti@gmail.com>
2017-07-07 21:03:06 -0400

    steps

--- formcatcher.php
+++ formcatcher.php
@@ -4,11 +4,11 @@
 
   $categories = htmlspecialchars($_POST['username']);
 
-  //$apiString = "WHERE Category IN ('" . $categories . "')";
+  $apiString = "WHERE Category IN ('" . $categories . "')";
   //$apiURL = urlencode($apiString);
 
   //$my_var = file_get_contents("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+ROWID%2C+Name%2C+Description+FROM+1Iw9qt8__F6E8RB53bbflrGAt3eI4GS3StvcF1o-I%20" . $apiURL . "&key=AIzaSyBYdqXls8CORbjl1yVC53qZ-75pMJaCmJY");
 
-  echo $categories;
+  echo $apiString;
   //echo $my_var;
 ?>
