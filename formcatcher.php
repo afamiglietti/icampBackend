f2a53d427c96eb2866c8779db0dba11bc8048468
Andrew Famiglietti <afamiglietti@gmail.com>
2017-07-07 20:41:07 -0400

    with html

--- formcatcher.php
+++ formcatcher.php
@@ -2,7 +2,9 @@
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
 
-  $apiString = "WHERE Category IN ('sports', 'media')";
+  $categories = htmlspecialchars($_POST['cats']);
+
+  $apiString = "WHERE Category IN ('" . $categories . "')";
   $apiURL = urlencode($apiString);
 
   $my_var = file_get_contents("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+ROWID%2C+Name%2C+Description+FROM+1Iw9qt8__F6E8RB53bbflrGAt3eI4GS3StvcF1o-I%20" . $apiURL . "&key=AIzaSyBYdqXls8CORbjl1yVC53qZ-75pMJaCmJY");
