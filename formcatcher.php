990984e5b2b004360fa039c4c37bf6ac9c45fda3
Andrew Famiglietti <afamiglietti@gmail.com>
2017-07-07 20:42:48 -0400

    test url

--- formcatcher.php
+++ formcatcher.php
@@ -9,5 +9,6 @@
 
   $my_var = file_get_contents("https://www.googleapis.com/fusiontables/v2/query?sql=SELECT+ROWID%2C+Name%2C+Description+FROM+1Iw9qt8__F6E8RB53bbflrGAt3eI4GS3StvcF1o-I%20" . $apiURL . "&key=AIzaSyBYdqXls8CORbjl1yVC53qZ-75pMJaCmJY");
 
+  echo $apiURL;
   echo $my_var;
 ?>
